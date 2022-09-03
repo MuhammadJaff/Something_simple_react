e473cbb321e27934f3d54213906a98b81d27d41d
Muhammad Jafar <muhammadjafarrahimjonzoda@gmail.com>
2022-09-03 15:51:33 +0500

    some styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import './App.css'
+import Header from './Components/Header/Header'
+import About from './Components/About/About'
+import Contact from './Components/Contact/Contact'
+import Experience from './Components/Experience/Experience'
+import Footer from './Components/Footer/Footer'
+import Nav from './Components/Nav/Nav'
+import Service from './Components/Service/Service'
+import Testimonial from './Components/Testimonial/Testimonial'
+import Portfolio from './Components/Portfolio/Portfolio'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      	<Header/>
+		<Nav/>
+		<About/>
+		<Experience/>
+		<Service/>
+		<Portfolio/>
+		<Testimonial/>
+		<Contact/>
+		<Footer/>		
+    </div>
   )
 }
 
